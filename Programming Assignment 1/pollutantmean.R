70fe8fb66acf4e19221d1ce954a7339a018ea613
UgarteNahia <53377224+UgarteNahia@users.noreply.github.com>
2021-02-27 19:32:18 +0100

    Create pollutantmean.R

---
+++ Programming Assignment 1/pollutantmean.R
@@ -0,0 +1,24 @@
+pollutantmean <- function  (poldir, poltype, id=1:332){
+  allfiles <- list.files (poldir, full.names=TRUE)
+  poldata <- lapply(allfiles, read.csv)
+  p <- poltype
+  n <- min(id)
+  l <- length(id)
+  total <- integer()
+  
+  
+  if (p== 'sulfate'){col <-2}
+  if (p== 'nitrate') {col <- 3}
+  
+  
+  for (r in 1:l){
+    column <- (data.frame(poldata[n]) [,col])
+    total <- append(total, column)
+    n <- n + 1
+      
+  }
+  polmean <- mean(total, na.rm= TRUE)
+  print(polmean)
+}
+
+© 2021 GitHub, Inc.
